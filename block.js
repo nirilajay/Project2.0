e10b3df469ecb54b2786b685e9c5ff5f81171d8e
nirilajay <nirilajay2317@gmail.com>
2023-06-17 16:29:39 +0530

    difficulty and nonce code

--- block.js
+++ block.js
@@ -2,27 +2,32 @@ const { GENESIS_DATA } = require('./config');
 const cryptoHash = require('./crypto-hash');
 
 class Block {
-  constructor({ timestamp, lastHash, hash, data }) {
+  constructor({ timestamp, lastHash, hash, data, nonce, difficulty }) {
     this.timestamp = timestamp;
     this.lastHash = lastHash;
     this.hash = hash;
     this.data = data;
+    this.nonce = nonce;
+    this.difficulty = difficulty;
   }
 
   static genesis() {
     return new this(GENESIS_DATA);
   }
 
-    static mineBlock({ lastBlock, data }) {
-        const timestamp = Date.now();
-        const lastHash = lastBlock.hash;
+  static mineBlock({ lastBlock, data }) {
+    let hash, timestamp;
+    const lastHash = lastBlock.hash;
+    const { difficulty } = lastBlock;
+    let nonce = 0;
 
-    return new this({
-      timestamp,
-      lastHash,
-        data,
-      hash: cryptoHash(timestamp, lastHash, data)
-    });
+    do {
+      nonce++;
+      timestamp = Date.now();
+      hash = cryptoHash(timestamp, lastHash, data, nonce, difficulty);
+    } while (hash.substring(0, difficulty) !== '0'.repeat(difficulty));
+
+    return new this({ timestamp, lastHash, data, difficulty, nonce, hash });
   }
 }
 
